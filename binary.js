96ecf6e50646c77b4737d4cba9cbd29f86b030ea
Nilesh Ranpise <78468875+NileshRanpise@users.noreply.github.com>
2022-02-18 21:43:50 +0530

    added new js file

---
+++ binary.js
@@ -0,0 +1,11 @@
+// Implement Insert functionality
+
+var insert = function(array, rightIndex, value) {
+    for(var j = rightIndex;
+        j >= 0 && array[j] > value;
+        j--) {
+        array[j + 1] = array[j];
+    }   
+    array[j + 1] = value; 
+};
+
